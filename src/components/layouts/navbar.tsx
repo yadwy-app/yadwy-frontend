6d78d570bf8430163c289000a8ac12ac39059e4e
ahmed <mommentcraft@gmail.com>
2025-02-05 01:22:53 +0200

    style navbar

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -14,11 +14,19 @@ import Link from "next/link";
 import { LinkIcon } from "../ui/link-icon";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { FaHeart } from "react-icons/fa";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu";
 
 export const Navbar = () => {
   return (
     <>
-      <div className="flex items-center justify-between w-full">
+      <div className="flex w-full items-center justify-between">
         <Link href={`/`} className="relative h-12 w-28">
           <Image src="/logo.svg" layout="fill" objectFit="contain" alt="" />
         </Link>
@@ -44,7 +52,9 @@ export const Navbar = () => {
           </div>
           <div className="flex items-center gap-3 border-x-2 border-gray-200 px-3">
             <div className="relative">
-              <div className="bg-red-500 flex justify-center items-center absolute w-[16px] h-[16px] top-[-10px] right-[0px] text-[9px] p-[2px] rounded-full text-white font-bold">2</div>
+              <div className="absolute right-[0px] top-[-10px] flex h-[16px] w-[16px] items-center justify-center rounded-full bg-red-500 p-[2px] text-[9px] font-bold text-white">
+                2
+              </div>
               <LinkIcon href="">
                 <MdOutlineShoppingBag className="text-1xl" />
               </LinkIcon>
@@ -53,6 +63,28 @@ export const Navbar = () => {
               <FaHeart className="text-1xl text-primary" />
             </LinkIcon>
           </div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="relative rounded-full">
+                  <Image
+                    src="/profile.svg"
+                    layout="fill"
+                    objectFit="contain"
+                    alt=""
+                  />
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="flex flex-col border-gray-200 border">
+                  <NavigationMenuLink className="w-full px-8 py-2 hover:bg-slate-200 transition-all">
+                    Profile
+                  </NavigationMenuLink>
+                  <NavigationMenuLink className="w-full px-8 py-2 hover:bg-slate-200 transition-all bg-slate-100 text-red-500">
+                    Logout
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
       </div>
     </>
