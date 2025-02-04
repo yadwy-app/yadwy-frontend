95d2a10196c738311bc9b78faa8f0a15f4580cfb
ahmed <mommentcraft@gmail.com>
2025-02-04 21:01:27 +0200

    structure css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,18 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={``}>
+      <head>
+        <style>
+          {`
+            :root {
+              --main-color: #6da0a0;
+              --sec-color: #e2ebeb;
+              --gray-color: #eef0f0;
+              --dark-color: #1e1e1e;
+            }
+          `}
+        </style>
+      </head>
       <body>
         <Navbar />
         {children}
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
-
+import './Navbar.css'
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="logo">
+        <Image src='/logo.svg' fill alt='' />
+      </div>
+    </div>
   )
 }
