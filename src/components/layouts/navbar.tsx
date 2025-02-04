f7ea1caed9d0907a5debbda757017b6762dc69cf
ahmed <mommentcraft@gmail.com>
2025-02-05 00:12:02 +0200

    style navbar

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -18,39 +18,40 @@ import { FaHeart } from "react-icons/fa";
 export const Navbar = () => {
   return (
     <>
-      <div className="">
-        <div className="flex items-center justify-between">
-          <Link href={`/`} className="relative h-12 w-28">
-            <Image src="/logo.svg" layout="fill" objectFit="contain" alt="" />
-          </Link>
-          <div className="flex gap-4">
-            <div className="flex items-center gap-1">
-              <div className="flex items-center">
-                <Select>
-                  <SelectTrigger className="w-[98px] rounded-sm rounded-r-none border border-gray-300 bg-gray-200">
-                    <SelectValue placeholder="All" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="--">--</SelectItem>
-                  </SelectContent>
-                </Select>
-                <Input
-                  className="rounded-sm rounded-l-none border border-gray-300"
-                  placeholder="Search for artwork, plants...."
-                />
-              </div>
-              <Button>
-                <CiSearch className="text-white"/>
-              </Button>
+      <div className="flex items-center justify-between">
+        <Link href={`/`} className="relative h-12 w-28">
+          <Image src="/logo.svg" layout="fill" objectFit="contain" alt="" />
+        </Link>
+        <div className="flex gap-4">
+          <div className="flex items-center gap-1">
+            <div className="flex items-center">
+              <Select>
+                <SelectTrigger className="w-[98px] rounded-sm rounded-r-none border border-gray-300 bg-gray-200">
+                  <SelectValue placeholder="All" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="--">--</SelectItem>
+                </SelectContent>
+              </Select>
+              <Input
+                className="rounded-sm rounded-l-none border border-gray-300"
+                placeholder="Search for artwork, plants...."
+              />
             </div>
-            <div className="flex gap-3 items-center border-x-2 px-3 border-gray-200">
-              <LinkIcon href=""><MdOutlineShoppingBag className="text-1xl" />
-              </LinkIcon>
+            <Button>
+              <CiSearch className="text-white" />
+            </Button>
+          </div>
+          <div className="flex items-center gap-3 border-x-2 border-gray-200 px-3">
+            <div className="relative">
+              <div className="bg-red-500 flex justify-center items-center absolute w-[16px] h-[16px] top-[-10px] right-[0px] text-[9px] p-[2px] rounded-full text-white font-bold">2</div>
               <LinkIcon href="">
-              <FaHeart  className="text-1xl text-primary" />
+                <MdOutlineShoppingBag className="text-1xl" />
               </LinkIcon>
             </div>
-            
+            <LinkIcon href="">
+              <FaHeart className="text-1xl text-primary" />
+            </LinkIcon>
           </div>
         </div>
       </div>
