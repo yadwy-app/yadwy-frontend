ec2b2082b2ede674cc434526a3460232e9ec3806
ahmed <mommentcraft@gmail.com>
2025-02-10 22:03:51 +0200

    style

--- src/app/(root)/_components/cart/cart-sheet.tsx
+++ src/app/(root)/_components/cart/cart-sheet.tsx
@@ -32,26 +32,24 @@ const products = [
 ];
 export const CartSheet = ({ children }: CartProps) => {
   return (
-    <div>
-      <Sheet>
-        {children}
-        <SheetContent className="p-0">
-          <SheetHeader className="p-5">
-            <SheetTitle className="border-b-2 border-gray-200 pb-2">
-              Shopping Cart
-            </SheetTitle>
-          </SheetHeader>
+    <Sheet >
+      {children}
+      <SheetContent className="p-0 w-100 md:w-auto">
+        <SheetHeader className="p-5">
+          <SheetTitle className="border-b-2 border-gray-200 pb-2">
+            Shopping Cart
+          </SheetTitle>
+        </SheetHeader>
 
-          <div className="relative h-full p-5 pb-[230px] pe-0 pt-0">
-            <div className="flex h-full max-h-[calc(100vh-230px)] flex-col gap-5 overflow-y-auto overflow-x-hidden pe-5">
-              {products.map((product) => (
-                <BoxProductCart item={product} key={product.id} />
-              ))}
-            </div>
-            <Control price={213} />
+        <div className="relative h-full p-5 pb-[230px] pe-0 pt-0">
+          <div className="flex h-full max-h-[calc(100vh-230px)] flex-col gap-5 overflow-y-auto overflow-x-hidden pe-5">
+            {products.map((product) => (
+              <BoxProductCart item={product} key={product.id} />
+            ))}
           </div>
-        </SheetContent>
-      </Sheet>
-    </div>
+          <Control price={213} />
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 };
