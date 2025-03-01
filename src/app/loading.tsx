d7c7c182699d9da6440c0fceaf642b3bd7cd3ad1
ahmed <mommentcraft@gmail.com>
2025-03-01 12:16:36 +0200

    first commit

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-red-600">
+    <div className="flex justify-center items-center h-screen ">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
