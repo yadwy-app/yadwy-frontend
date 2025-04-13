60bdb88162aa13ac5704d198685612fb838f89bc
ahmed <mommentcraft@gmail.com>
2025-04-13 22:49:49 +0200

    fix : remove console.log statement from PhoneMenu component

--- src/components/layouts/phone-menu.tsx
+++ src/components/layouts/phone-menu.tsx
@@ -23,7 +23,6 @@ const PhoneMenu = () => {
   const pathname = usePathname();
   const locale = useLocale();
   const items = navigationItems[locale as keyof typeof navigationItems];
-  console.log( pathname);
   return (
     <div className="fixed md:hidden bottom-0 left-0 right-0 bg-white border-t border-gray-200 py-2 z-50">
       <div className="grid grid-cols-4 gap-4">
