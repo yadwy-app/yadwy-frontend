3d45d1b98500c2a556516014f83d3b060d099941
Mohamed-Ahmed-Yousef <modyyousef800@gmail.com>
2025-04-24 23:06:38 +0200

    feat : exclude shadcn from checking in biome and ts

--- apps/seller/src/lib/product-utils.ts
+++ apps/seller/src/lib/product-utils.ts
@@ -1,4 +1,4 @@
-import type { Product } from "~/types";
+import type { Product } from "@/types";
 
 export function getProductCoverImage(product: Pick<Product, "images">) {
   return product.images[0] || "/placeholder.svg";
